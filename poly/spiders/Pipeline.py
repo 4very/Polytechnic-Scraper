02b51dd41080dd2fc95723fe5b22d6621be8e0fe
Avery <asommer999@gmail.com>
2019-12-04 20:39:40 -0500

    initial commit

---
+++ poly/spiders/Pipeline.py
@@ -0,0 +1,49 @@
+import scrapy
+from w3lib.html import remove_tags
+
+class PipelineSpider(scrapy.Spider):
+
+    name = "Pipeline"
+    start_urls = [
+        'https://poly.rpi.edu/archives/'
+    ]
+
+
+    def parse(self, response):
+
+        if (response.url == "https://poly.rpi.edu/archives/"):
+            rel_links = response.css("section.content a.px-2::attr(href)").extract()
+            for link in rel_links:
+                print(response.urljoin(link))
+                yield scrapy.Request(url=response.urljoin(link))
+        
+        elif (response.css("h1 a::text").extract_first() == "Archives"):
+            print("Archive: ", response.url)
+            rel_links = response.css("h3.headline a::attr(href)").extract()
+            for link in rel_links:
+                print(response.urljoin(link))
+                yield scrapy.Request(url=response.urljoin(link))
+        
+        else:
+            print("Non Archive: ", response.url)
+            yield {
+                'section': response.css("a.active::text").extract_first(),
+                'kicker': response.css("strong.text-kicker::text").extract_first(),
+                'headline': remove_tags("".join(response.css("h1.article-headline").re(r'<h1.*?>(.*?)<\/h1>'))),
+                'author': response.css(".author-name::text").extract(),
+                'posted-date': response.css("a.published-date::text").extract_first().strip(),
+                 
+                'has-featured-photo': response.css("div.featured-photo img::attr(src)").extract_first() is not None,
+                'featured-photo-photographer': response.css("div.featured-photo span.small a::text").extract_first(),
+                'featured-photo-caption': remove_tags("".join(response.css("span.caption").re(r'<span.*?>(.*?)<\/span>'))),
+                
+                'has-gallery': response.css("div.photo-gallery::text").extract_first() is not None,
+                'gallery-photographers': response.css("div.photo-gallery div.photo img::attr(data-photographer)").re(r'<a.*>(.*?)<\/a>'),
+                'gallery-captions': response.css("div.photo-gallery div.photo img::attr(data-caption)").extract(),
+                
+                'num-block-photos': len(response.css("div.photo-block").extract()),
+                'block-photographers': response.css("div.photo-block div.container span.small a::text").extract(),
+                'block-captions': response.css("div.photo-block div.container span.caption::text").extract(),
+                
+                'body' : remove_tags(" ".join(response.css("div.body p").extract())).replace("\n",''),
+            }
